e2a6ac4a6141305c700742bcbd4bce1ff2d6ac35
baxterjo <j.bax080@gmail.com>
2025-05-16 21:25:51 -0700

    Troubleshooting styling errors

--- src/app.rs
+++ src/app.rs
@@ -9,11 +9,11 @@ pub fn app() -> Element {
     info!("Initialized site with content: {site_content:#?}");
     use_context_provider(|| Signal::new(site_content));
     rsx! {
-        document::Script { src: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" }
-        document::Stylesheet { href: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" }
-        document::Stylesheet { href: "https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700" }
-        document::Stylesheet { href: asset!("public/css/style.css") }
-        document::Stylesheet { href: asset!("public/css/font-awesome/css/font-awesome.min.css") }
+        //document::Script { src: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" }
+        //document::Stylesheet { href: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" }
+        //document::Stylesheet { href: "https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700" }
+        //document::Stylesheet { href: asset!("public/css/style.css") }
+        //document::Stylesheet { href: asset!("public/css/font-awesome/css/font-awesome.min.css") }
         Router::<Route> {}
     }
 }
